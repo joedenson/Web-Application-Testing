71856a3eebe886fa4d14a60f35ad920901fd1da2
Joe Denson <joedenson79@gmail.com>
2019-10-03 21:13:59 -0700

    second commit

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -33,11 +33,42 @@ export const addHit = currentValue => {
     return currentValue = 0;
 };
 
+export const hitCount = currentHits => {
+    return currentHits + 1;
+}
+
+
 
 
 function Dashboard() {
     const [strikes, setStrikes] = useState(0);
     const [balls, setBalls] = useState(0);
+    const [hits, setHits] = useState(0);
+
+    function handleBalls() {
+        setBalls(addBall(balls));
+        setStrikes(addStrike(strikes));
+        setStrikes(addFoul(strikes));
+
+    }
+
+    function handleBalls() {
+        setBalls(addBall(balls));
+    }
+
+    function handleStrikes() {
+        setStrikes(addStrike(strikes));
+    }
+
+    function handleFouls() {
+        setStrikes(addFoul(strikes));
+    }
+
+    function handleHits() {
+        setStrikes(addHit(strikes));
+        setBalls(addHit(balls));
+        setHits(hitCount(hits));
+    }
 
 
     return (
@@ -46,13 +77,14 @@ function Dashboard() {
             <div className="score-container">
                 <div className='balls'>Balls: {balls}</div>
                 <div className='strikes'>Strikes: {strikes}</div>
-
+                <div className='hitcount'>Hits: {hits}</div>
             </div>
             <div className='dashboard'>
-                <button className='ball' onClick={() => setBalls(addBall(balls))}>Ball</button>
-                <button className='strike' onClick={() => setStrikes(addStrike(strikes))}>Strike</button>
-                <button className='foul' onClick={() => setStrikes(addFoul(strikes))}>Foul</button>
-                <button className='hit' onClick={() => setStrikes(addHit(strikes), setBalls(addHit(balls)))}>Hit</button>
+                <button className='ball' onClick={handleBalls}>Ball</button>
+                <button className='strike' onClick={handleStrikes}>Strike</button>
+                <button className='foul' onClick={handleFouls}>Foul</button>
+                <button className='hit' onClick={handleHits}>Hit</button>
+
             </div>
         </div>
 
